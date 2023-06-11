b515dc386d8d274d66aa6dec57f0f23d690c5369
ALBKIN <kinkel.albert@gmail.com>
2023-06-11 14:21:42 +0200

    11.06.2023

--- src/main/java/com/albkin/diningreviewapi/model/DiningReview.java
+++ src/main/java/com/albkin/diningreviewapi/model/DiningReview.java
@@ -1,37 +1,29 @@
 package com.albkin.diningreviewapi.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;;
 
 @Entity
-@Table(name="DINING_REVIEWS")
-@Data
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class DiningReview {
 
     @Id
     @GeneratedValue
     private Long id;
 
-    @Column(name="REVIEW_BY")
     private String reviewByUsername;
-
-    @Column(name="RESTAURANT_ID")
     private Long RestaurantId;
-
-    @Column(name="PEANUT_SCORE")
     private Integer peanutScore;
-
-    @Column(name="EGG_SCORE")
     private Integer eggScore;
-
-    @Column(name="DAIRY_SCORE")
     private Integer dairyScore;
-
-    @Column(name="COMMENTARY")
     private String commentary;
-
-    @Column(name="REVIEW_STATUS")
     private ReviewStatus reviewStatus;
-
 
 }
