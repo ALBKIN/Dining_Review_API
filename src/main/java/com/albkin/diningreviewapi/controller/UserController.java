6a0e8db9f44da544caee81994968df02882641b6
ALBKIN <kinkel.albert@gmail.com>
2023-06-15 22:52:03 +0200

    14.06.2023

--- src/main/java/com/albkin/diningreviewapi/controller/UserController.java
+++ src/main/java/com/albkin/diningreviewapi/controller/UserController.java
@@ -21,11 +21,26 @@ public class UserController {
     public Iterable<User> getAllUsers() {
         return this.userRepository.findAll();
     }
+
     // method to fetch user by ID
     @GetMapping("/users/{id}")
     public Optional<User> getUserById(@PathVariable("id") Long id) {
         return this.userRepository.findById(id);
     }
+
+    //  method to fetch user by username
+    @GetMapping("/users/{username}")
+    private User getUserByName(@PathVariable("username") String username) {
+        confirmUserName(username);
+
+        Optional<User> optionalUser = this.userRepository.findUserByUserName(username);
+        if (!optionalUser.isPresent()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Username does not exist");
+        }
+        User existingUser = optionalUser.get();
+        return existingUser;
+    }
+
     // method to create new user
     @PostMapping("/users")
     @ResponseStatus(HttpStatus.CREATED)
@@ -35,6 +50,28 @@ public class UserController {
       return user;  // OPTIONAL?
     }
 
+    // method to update existing user, but username cannot be modified
+    @PutMapping("/users/{username}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    private User updateUserData(@PathVariable("username") String userName, @RequestBody User updatedUser) {
+        confirmUserName(userName);
+
+        Optional<User> checkUserToUpdate = this.userRepository.findUserByUserName(userName);
+        if (checkUserToUpdate.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NO_CONTENT, "Can't update non-existing uesr");
+        }
+        User existingUser = checkUserToUpdate.get();
+        copyUserInfoFrom(updatedUser, existingUser);
+        this.userRepository.save(existingUser);
+        return existingUser;
+
+    }
+
+    private void copyUserInfoFrom(User updatedUser, User existingUser) {
+
+    }
+
+    // method to check if whether user already exists
     private void confirmUser(User user) {
         confirmUserName(user.getUserName());
 
@@ -44,6 +81,7 @@ public class UserController {
         }
     }
 
+    // helper method used to verify that the display name provided by the user is not null or empty
     private void confirmUserName(String userName) {
         if (ObjectUtils.isEmpty(userName)) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username cannot be null or empty");
