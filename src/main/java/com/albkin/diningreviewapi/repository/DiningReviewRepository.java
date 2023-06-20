0be81b2f00fddae82cbbc681a2480c834cb9fde0
ALBKIN <kinkel.albert@gmail.com>
2023-06-20 14:53:04 +0200

    20.06.2023

--- src/main/java/com/albkin/diningreviewapi/repository/DiningReviewRepository.java
+++ src/main/java/com/albkin/diningreviewapi/repository/DiningReviewRepository.java
@@ -1,7 +1,11 @@
 package com.albkin.diningreviewapi.repository;
 
-import org.springframework.data.repository.CrudRepository;
 import com.albkin.diningreviewapi.model.DiningReview;
+import com.albkin.diningreviewapi.model.ReviewStatus;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface DiningReviewRepository extends CrudRepository<DiningReview, Long> {
+    List<DiningReview> findByReviewStatus(ReviewStatus reviewStatus);
 }
